7a074782ac3afefcd0a0a4dd22c3a7a6c4855a45
Abhishek Walia <abhishek@confluent.io>
2019-12-18 16:23:14 -0800

    Added Check for Already present Keys, CSR's and Certificates before generating new CSR's for the same host again

--- LeafUtils/issueCSRs.sh
+++ LeafUtils/issueCSRs.sh
@@ -31,22 +31,34 @@ then
     #### Create config for use in CSR Request accepted format
     echo "subjectAltName = $DNS_NAMES_STRING" > $TEMP_FILE_NAME
 
-    # Create the Certificate Key
-    openssl genrsa \
-      -aes256 \
-      -passout file:$KEY_PASS_FILE_NAME \
-      -out $CN_NAME_STRING.key \
-      4096
+    if [ -e $CN_NAME_STRING.key ]
+    then
+      echo "The Key for $CN_NAME_STRING already exists. Not Regenerating Key."
+    else
+      # Create the Certificate Key
+      openssl genrsa \
+        -aes256 \
+        -passout file:$KEY_PASS_FILE_NAME \
+        -out $CN_NAME_STRING.key \
+        4096
+    fi
+
     # Create one single file with all the configs ( tried cat withount new file, but openssl was flaky
     cat $CSRCONF_FILE_NAME $TEMP_FILE_NAME > $CONCAT_FILE_NAME
-    # Generate CSR Request
-    openssl req \
-      -new \
-      -sha256 \
-      -out $CN_NAME_STRING.csr \
-      -key $CN_NAME_STRING.key \
-      -passin file:"$KEY_PASS_FILE_NAME" \
-      -config $CONCAT_FILE_NAME
+
+    if [ -e $CN_NAME_STRING.csr ] || [ -e $CN_NAME_STRING.pem ]
+    then
+      echo "The CSR or Certificate for $CN_NAME_STRING already exists. Not Regenerating CSR."
+    else
+      # Generate CSR Request
+      openssl req \
+        -new \
+        -sha256 \
+        -out $CN_NAME_STRING.csr \
+        -key $CN_NAME_STRING.key \
+        -passin file:"$KEY_PASS_FILE_NAME" \
+        -config $CONCAT_FILE_NAME
+    fi
 
   done
 
