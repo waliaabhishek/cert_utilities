5eed440bf2b91074bcadaae9890ee36afcc7e871
Abhishek Walia <abhishek@confluent.io>
2020-11-06 14:49:21 -0800

    Change filename for Root Cert Private Key

--- LeafUtils/cleanOutDir.sh
+++ LeafUtils/cleanOutDir.sh
@@ -6,7 +6,7 @@ baseDir=`basename \`pwd\``
 if [ $baseDir = $correctBaseDir ]
 then
   echo "Removing Old Root Certificate and Key"
-  rm ./*.crt ./*.csr ./*.pem 2>/dev/null
+  rm ./*.crt ./*.csr ./*.pem ./*.key 2>/dev/null
   rm ..pem 2>/dev/null
 
   echo "Clean out temp remnants ( if any )"
